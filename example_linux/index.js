39bee0bd1ba2e44d567c2d137028306c91dd23fc
Simon Hofmann <mail@simon-hofmann.org>
2018-10-29 22:47:15 +0100

    Sync

--- example_linux/index.js
+++ example_linux/index.js
@@ -1,28 +1,28 @@
-'use strict'
+'use strict';
 
-const nut = require("../lib/nut");
+const nut = require('../lib/nut');
 
 nut.MATCH_PROB = 0.93;
 
-console.log(nut.Screen.location("./docker.png"));
+console.log(nut.Screen.location('./docker.png'));
 console.log(nut.Screen.width());
 console.log(nut.Screen.height());
-nut.Mouse.move(nut.Movement.straightTo(nut.Location.centerOf(nut.Screen.location("./file.png"))));
+nut.Mouse.move(nut.Movement.straightTo(nut.Location.centerOf(nut.Screen.location('./file.png'))));
 nut.Mouse.leftClick();
-nut.Mouse.move(nut.Movement.straightTo(nut.Location.centerOf(nut.Screen.location("./save.png"))));
+nut.Mouse.move(nut.Movement.straightTo(nut.Location.centerOf(nut.Screen.location('./save.png'))));
 nut.Mouse.leftClick();
-nut.Mouse.move(nut.Movement.straightTo(nut.Location.centerOf(nut.Screen.location("./save_button.png"))));
+nut.Mouse.move(nut.Movement.straightTo(nut.Location.centerOf(nut.Screen.location('./save_button.png'))));
 nut.Mouse.leftClick();
 
-nut.Mouse.move(nut.Movement.straightTo(nut.Location.randomPointAt(nut.Screen.location("./docker.png"))));
+nut.Mouse.move(nut.Movement.straightTo(nut.Location.randomPointAt(nut.Screen.location('./docker.png'))));
 
 for (let idx = 0; idx < 5; ++idx) {
-  nut.Mouse.move(nut.Movement.up(50));
-  nut.Mouse.leftClick();
+    nut.Mouse.move(nut.Movement.up(50));
+    nut.Mouse.leftClick();
 }
 
 try {
-  nut.Assert.isVisible("./docker.png", new nut.Area(100, 100, 100, 100));
+    nut.Assert.isVisible('./docker.png', new nut.Region(100, 100, 100, 100));
 } catch (err) {
-  console.log(err.message);
+    console.log(err.message);
 }
--- example_macos/index.js
+++ example_macos/index.js
@@ -1,26 +1,52 @@
-'use strict'
+'use strict';
 
-const nut = require("../lib/nut");
+const nut = require('../lib/nut');
 
-console.log(nut.Screen.location("./docker.png"));
+nut.MATCH_PROB = 0.95;
+
+console.log(nut.Screen.location('./docker.png'));
 console.log(nut.Screen.width());
 console.log(nut.Screen.height());
-nut.Mouse.move(nut.Movement.straightTo(nut.Location.centerOf(nut.Screen.location("./file.png"))));
+nut.Mouse.move(
+    nut.Movement.straightTo(
+        nut.Location.centerOf(
+            nut.Screen.location('./file.png')
+        )
+    )
+);
 nut.Mouse.leftClick();
-nut.Mouse.move(nut.Movement.straightTo(nut.Location.centerOf(nut.Screen.location("./save.png"))));
-nut.Mouse.leftClick();
-nut.Mouse.move(nut.Movement.down(220));
+nut.Mouse.move(
+    nut.Movement.straightTo(
+        nut.Location.centerOf(
+            nut.Screen.location('./save.png')
+        )
+    )
+);
 nut.Mouse.leftClick();
 
-nut.Mouse.move(nut.Movement.straightTo(nut.Location.centerOf(nut.Screen.location("./docker.png"))));
+nut.Mouse.move(
+    nut.Movement.straightTo(
+        nut.Location.centerOf(
+            nut.Screen.location('./docker.png')
+        )
+    )
+);
 
 for (let idx = 0; idx < 5; ++idx) {
-  nut.Mouse.move(nut.Movement.up(50));
-  nut.Mouse.leftClick();
+    nut.Mouse.move(
+        nut.Movement.up(50)
+    );
+    nut.Mouse.leftClick();
 }
 
 try {
-  nut.Assert.isVisible("./docker.png", new nut.Area(100, 100, 100, 100));
+    nut.Assert.isVisible('./docker.png', new nut.Region(100, 100, 100, 100));
 } catch (err) {
-  console.log(err.message);
+    console.log(err.message);
+}
+
+try {
+    nut.Assert.notVisible('./docker.png', new nut.Region(0, 247, 51, 50));
+} catch (err) {
+    console.log(err.message);
 }
