0a2e904f8ef707c399d6be95721cff01a7e6acb6
Simon Hofmann <mail@simon-hofmann.org>
2019-01-02 00:10:43 +0100

    Updated Jest config
    Adder coverage config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
-    collectCoverage: true,
+    collectCoverageFrom: [
+        "index.ts",
+        "lib/**/*.ts",
+        "!<rootDir>/node_modules/",
+        "!<rootDir>/path/to/dir/"
+    ],
+    coverageReporters: ["text"],
 };
