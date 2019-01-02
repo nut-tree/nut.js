cd12126d03fa27da6db3de0fb42ef2b8df4524e3
Simon Hofmann <mail@simon-hofmann.org>
2019-01-02 20:58:50 +0100

    #4 - Added pixel scaling compensation
    High resolution displays like Apples Retina displays have a higher pixel resolution than actually reported by the OS. When dealing with mouse movement, this scaling has to be taken care of

--- lib/region.class.ts
+++ lib/region.class.ts
@@ -1,4 +1,16 @@
 export class Region {
+
+    public static scaled(region: Region, scaleX: number = 1.0, scaleY: number = 1.0): Region {
+        if (scaleX === 0 || scaleY === 0) {
+            throw new Error(`Scaling to 0. Please check parameters: scaleX: ${scaleX}, scaleY: ${scaleY}`);
+        }
+        return new Region(
+            region.left * scaleX,
+            region.top * scaleY,
+            region.width * scaleX,
+            region.height * scaleY);
+    }
+
     constructor(public left: number, public top: number, public width: number, public height: number) {
     }
 
