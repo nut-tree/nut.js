5f3f7b4ea559f1c7f8f0441931a84206d2743b1f
Simon Hofmann <mail@simon-hofmann.org>
2019-01-21 23:16:14 +0100

    Added clipboard class

---
+++ lib/clipboard.class.ts
@@ -0,0 +1,14 @@
+import { NativeAdapter } from "./adapter/native.adapter.class";
+
+export class Clipboard {
+    constructor(private nativeAdapter: NativeAdapter) {
+    }
+
+    public copy(text: string): void {
+        this.nativeAdapter.copy(text);
+    }
+
+    public paste(): string {
+        return this.nativeAdapter.paste();
+    }
+}
