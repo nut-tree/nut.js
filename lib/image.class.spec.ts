8836a1110e8a5d9f0d066fb011604c0d20ca8b10
Simon Hofmann <mail@simon-hofmann.org>
2019-01-29 14:47:32 +0100

    Added channel info to Image type

---
+++ lib/image.class.spec.ts
@@ -0,0 +1,25 @@
+import { Image } from "./image.class";
+
+describe("Image class", () => {
+    it("should return alphachannel = true for > 3 channels", () => {
+        const SUT = new Image(200, 200, 123, 4);
+        expect(SUT.hasAlphaChannel).toBeTruthy();
+    });
+
+    it("should return alphachannel = false for <= 3 channels", () => {
+        const SUT = new Image(200, 200, 123, 3);
+        expect(SUT.hasAlphaChannel).toBeFalsy();
+    });
+    it("should return alphachannel = false for <= 3 channels", () => {
+        const SUT = new Image(200, 200, 123, 2);
+        expect(SUT.hasAlphaChannel).toBeFalsy();
+    });
+    it("should return alphachannel = false for <= 3 channels", () => {
+        const SUT = new Image(200, 200, 123, 1);
+        expect(SUT.hasAlphaChannel).toBeFalsy();
+    });
+
+    it("should throw for <= 0 channels", () => {
+        expect(() => new Image(200, 200, 123, 0)).toThrowError("Channel <= 0");
+    });
+});
