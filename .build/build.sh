bf7865f5b100de8641bbad27b6bc7238d1d20826
Simon Hofmann <mail@simon-hofmann.org>
2019-02-12 14:24:09 +0100

    Updated build script

--- .build/build.sh
+++ .build/build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
+set -e
 
-source ~/.nvmrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
 cd /nut
+nvm install lts/dubnium
 npm ci
 npm run compile
 npm run coverage
