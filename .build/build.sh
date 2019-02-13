5496b4357d464e31e2ebfcd0c8428b9d4202b1b3
Simon Hofmann <mail@simon-hofmann.org>
2019-02-13 08:09:48 +0100

    Log output

--- .build/build.sh
+++ .build/build.sh
@@ -4,8 +4,16 @@ set -e
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-cd /nut
+targetDir="/nut"
+nodeVersion="lts/dubnium"
+
+echo "Entering working directory"
+cd $targetDir
+echo "Installing node version $nodeVersion"
 nvm install lts/dubnium
+echo "npm ci"
 npm ci
+echo "npm run compile"
 npm run compile
+echo "npm run coverage"
 npm run coverage
