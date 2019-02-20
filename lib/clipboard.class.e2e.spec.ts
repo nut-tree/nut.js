77c596bbe664d8442e18536aa8af7e603e652273
Simon Hofmann <mail@simon-hofmann.org>
2019-02-20 01:16:11 +0100

    Updated E2E test to async API

--- lib/clipboard.class.e2e.spec.ts
+++ lib/clipboard.class.e2e.spec.ts
@@ -2,13 +2,13 @@ import {NativeAdapter} from "./adapter/native.adapter.class";
 import {Clipboard} from "./clipboard.class";
 
 describe("Clipboard class", () => {
-    it("should paste copied input from system clipboard.", () => {
+    it("should paste copied input from system clipboard.", async () => {
         const adapterMock = new NativeAdapter();
         const SUT = new Clipboard(adapterMock);
 
         const textToCopy = "bar";
 
         SUT.copy(textToCopy);
-        expect(SUT.paste()).toEqual(textToCopy);
+        expect(await SUT.paste()).toEqual(textToCopy);
     });
 });
