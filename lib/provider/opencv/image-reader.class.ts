d37a149eb525622ee07d0ce44143aadd3a7ec118
Simon Hofmann <mail@simon-hofmann.org>
2019-02-28 19:24:39 +0100

    Set imread to unchanged mode

--- lib/provider/opencv/image-reader.class.ts
+++ lib/provider/opencv/image-reader.class.ts
@@ -6,7 +6,7 @@ export class ImageReader implements DataSource {
   public async load(path: string): Promise<Image> {
     return new Promise<Image>(async (resolve, reject) => {
       try {
-        const image = await cv.imreadAsync(path);
+        const image = await cv.imreadAsync(path, cv.IMREAD_UNCHANGED);
         resolve(new Image(image.cols, image.rows, image.getData(), image.channels));
       } catch (e) {
         reject(`Failed to load image from '${path}'`);
