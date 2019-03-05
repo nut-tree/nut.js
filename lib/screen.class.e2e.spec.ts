327a3d26404ef3a529d17c1fa3c4aa574ff0d196
Simon Hofmann <mail@simon-hofmann.org>
2019-03-05 11:55:49 +0100

    Added further Screen E2E tests

--- lib/screen.class.e2e.spec.ts
+++ lib/screen.class.e2e.spec.ts
@@ -32,4 +32,54 @@ describe("Screen.", () => {
     await sleep(1000);
     expect(existsSync(filename)).toBeTruthy();
   });
+
+  it("should capture the screen and save file with prefix", async () => {
+    // GIVEN
+    const visionAdapter = new VisionAdapter();
+    const SUT = new Screen(visionAdapter);
+    const prefix = "foo_";
+
+    // WHEN
+    const filename = await SUT.capture("asdf.txt", FileType.JPG, "./", prefix);
+
+    // THEN
+    expect(filename.includes(prefix)).toBeTruthy();
+    expect(filename).not.toBeNull();
+    await sleep(1000);
+    expect(existsSync(filename)).toBeTruthy();
+  });
+
+  it("should capture the screen and save file with postfix", async () => {
+    // GIVEN
+    const visionAdapter = new VisionAdapter();
+    const SUT = new Screen(visionAdapter);
+    const postfix = "_bar";
+
+    // WHEN
+    const filename = await SUT.capture("asdf.txt", FileType.JPG, "./", "", postfix);
+
+    // THEN
+    expect(filename.includes(postfix)).toBeTruthy();
+    expect(filename).not.toBeNull();
+    await sleep(1000);
+    expect(existsSync(filename)).toBeTruthy();
+  });
+
+  it("should capture the screen and save file with pre- and postfix", async () => {
+    // GIVEN
+    const visionAdapter = new VisionAdapter();
+    const SUT = new Screen(visionAdapter);
+    const filename = "asdf";
+    const prefix = "foo_";
+    const postfix = "_bar";
+
+    // WHEN
+    const output = await SUT.capture("asdf", FileType.JPG, "./", prefix, postfix);
+
+    // THEN
+    expect(output.includes(`${prefix}${filename}${postfix}`)).toBeTruthy();
+    expect(output).not.toBeNull();
+    await sleep(1000);
+    expect(existsSync(output)).toBeTruthy();
+  });
 });
