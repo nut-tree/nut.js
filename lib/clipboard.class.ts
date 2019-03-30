47a47c44caaeabdb19d4b4344880755ec72abb6b
Simon Hofmann <mail@simon-hofmann.org>
2019-03-30 22:47:18 +0100

    (#47) Refactored clipboard class

--- lib/clipboard.class.ts
+++ lib/clipboard.class.ts
@@ -1,13 +1,11 @@
 import { NativeAdapter } from "./adapter/native.adapter.class";
 
 export class Clipboard {
-  constructor(private nativeAdapter: NativeAdapter) {}
+  constructor(private nativeAdapter: NativeAdapter) {
+  }
 
   public copy(text: string): Promise<void> {
-    return new Promise<void>(resolve => {
-      this.nativeAdapter.copy(text);
-      resolve();
-    });
+    return this.nativeAdapter.copy(text);
   }
 
   public paste(): Promise<string> {
