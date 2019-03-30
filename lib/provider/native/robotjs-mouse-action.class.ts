dabb0dcb5d2b0e26eae54435a1cc85c61a063b68
Simon Hofmann <mail@simon-hofmann.org>
2019-03-30 22:45:54 +0100

    (#47) Refactored mouse provider

--- lib/provider/native/robotjs-mouse-action.class.ts
+++ lib/provider/native/robotjs-mouse-action.class.ts
@@ -12,54 +12,131 @@ export class MouseAction implements MouseActionInterface {
     [[Button.LEFT, "left"], [Button.MIDDLE, "middle"], [Button.RIGHT, "right"]],
   );
 
-  constructor() {}
+  constructor() {
+  }
 
   public setMouseDelay(delay: number): void {
     robot.setMouseDelay(delay);
   }
 
-  public setMousePosition(p: Point): void {
-    robot.moveMouse(p.x, p.y);
+  public setMousePosition(p: Point): Promise<void> {
+    return new Promise<void>(((resolve, reject) => {
+      try {
+        robot.moveMouse(p.x, p.y);
+        resolve();
+      } catch (e) {
+        reject(e);
+      }
+    }));
   }
 
   public currentMousePosition(): Promise<Point> {
-    const position = robot.getMousePos();
-    return Promise.resolve(new Point(position.x, position.y));
+    return new Promise<Point>(((resolve, reject) => {
+      try {
+        const position = robot.getMousePos();
+        resolve(new Point(position.x, position.y));
+      } catch (e) {
+        reject(e);
+      }
+    }));
   }
 
-  public leftClick(): void {
-    robot.mouseClick(MouseAction.buttonLookup(Button.LEFT));
+  public leftClick(): Promise<void> {
+    return new Promise<void>(((resolve, reject) => {
+      try {
+        robot.mouseClick(MouseAction.buttonLookup(Button.LEFT));
+        resolve();
+      } catch (e) {
+        reject(e);
+      }
+    }));
   }
 
-  public rightClick(): void {
-    robot.mouseClick(MouseAction.buttonLookup(Button.RIGHT));
+  public rightClick(): Promise<void> {
+    return new Promise<void>(((resolve, reject) => {
+      try {
+        robot.mouseClick(MouseAction.buttonLookup(Button.RIGHT));
+        resolve();
+      } catch (e) {
+        reject(e);
+      }
+    }));
   }
 
-  public middleClick(): void {
-    robot.mouseClick(MouseAction.buttonLookup(Button.MIDDLE));
+  public middleClick(): Promise<void> {
+    return new Promise<void>(((resolve, reject) => {
+      try {
+        robot.mouseClick(MouseAction.buttonLookup(Button.MIDDLE));
+        resolve();
+      } catch (e) {
+        reject(e);
+      }
+    }));
   }
 
-  public pressButton(btn: Button): void {
-    robot.mouseToggle("down", MouseAction.buttonLookup(btn));
+  public pressButton(btn: Button): Promise<void> {
+    return new Promise<void>(((resolve, reject) => {
+      try {
+        robot.mouseToggle("down", MouseAction.buttonLookup(btn));
+        resolve();
+      } catch (e) {
+        reject(e);
+      }
+    }));
   }
 
-  public releaseButton(btn: Button): void {
-    robot.mouseToggle("up", MouseAction.buttonLookup(btn));
+  public releaseButton(btn: Button): Promise<void> {
+    return new Promise<void>(((resolve, reject) => {
+      try {
+        robot.mouseToggle("up", MouseAction.buttonLookup(btn));
+        resolve();
+      } catch (e) {
+        reject(e);
+      }
+    }));
   }
 
-  public scrollUp(amount: number): void {
-    robot.scrollMouse(0, amount);
+  public scrollUp(amount: number): Promise<void> {
+    return new Promise<void>(((resolve, reject) => {
+      try {
+        robot.scrollMouse(0, amount);
+        resolve();
+      } catch (e) {
+        reject(e);
+      }
+    }));
   }
 
-  public scrollDown(amount: number): void {
-    robot.scrollMouse(0, -amount);
+  public scrollDown(amount: number): Promise<void> {
+    return new Promise<void>(((resolve, reject) => {
+      try {
+        robot.scrollMouse(0, -amount);
+        resolve();
+      } catch (e) {
+        reject(e);
+      }
+    }));
   }
 
-  public scrollLeft(amount: number): void {
-    robot.scrollMouse(-amount, 0);
+  public scrollLeft(amount: number): Promise<void> {
+    return new Promise<void>(((resolve, reject) => {
+      try {
+        robot.scrollMouse(-amount, 0);
+        resolve();
+      } catch (e) {
+        reject(e);
+      }
+    }));
   }
 
-  public scrollRight(amount: number): void {
-    robot.scrollMouse(amount, 0);
+  public scrollRight(amount: number): Promise<void> {
+    return new Promise<void>(((resolve, reject) => {
+      try {
+        robot.scrollMouse(amount, 0);
+        resolve();
+      } catch (e) {
+        reject(e);
+      }
+    }));
   }
 }
