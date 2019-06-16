90e8dd641ccc40a0c9c07f969d3a6ba57c948a07
Simon Hofmann <mail@simon-hofmann.org>
2019-06-16 21:39:48 +0200

    (#68) Updated scaleImage function to use lowerBound function

--- lib/provider/opencv/scale-image.function.ts
+++ lib/provider/opencv/scale-image.function.ts
@@ -1,8 +1,9 @@
 import * as cv from "opencv4nodejs-prebuilt";
+import { lowerBound } from "./bound-value.function";
 
 export const scaleImage = async (image: cv.Mat, scaleFactor: number): Promise<cv.Mat> => {
-  const minScaleFactor = (scaleFactor <= 0.0) ? 1.0 : scaleFactor;
-  const scaledRows = Math.floor(image.rows * minScaleFactor);
-  const scaledCols = Math.floor(image.cols * minScaleFactor);
+  const boundScaleFactor = lowerBound(scaleFactor, 0.0, 1.0);
+  const scaledRows = Math.floor(image.rows * boundScaleFactor);
+  const scaledCols = Math.floor(image.cols * boundScaleFactor);
   return image.resizeAsync(scaledRows, scaledCols, 0, 0, cv.INTER_AREA);
 };
