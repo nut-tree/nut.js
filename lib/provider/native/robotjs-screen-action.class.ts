da531633aabfda4775a4ce438e71eb3719c6c0dd
Simon Hofmann <mail@simon-hofmann.org>
2019-09-25 13:48:45 +0200

    (#86) Updated screen action to use libnut

--- lib/provider/native/robotjs-screen-action.class.ts
+++ lib/provider/native/robotjs-screen-action.class.ts
@@ -1,4 +1,4 @@
-import robot = require("robotjs-node10");
+import robot = require("@nut-tree/libnut");
 import { Image } from "../../image.class";
 import { Region } from "../../region.class";
 import { ScreenActionProvider } from "./screen-action-provider.interface";
