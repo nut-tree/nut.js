ddc676b14f6ba5cba5b4d91bce53332a63e56755
Simon Hofmann <mail@simon-hofmann.org>
2019-10-07 15:56:08 +0200

    (#87) Location API docs

--- lib/location.function.ts
+++ lib/location.function.ts
@@ -1,6 +1,10 @@
 import { Point } from "./point.class";
 import { Region } from "./region.class";
 
+/**
+ * {@link centerOf} returns the center {@link Point} for a given {@link Region}
+ * @param target {@link Region} to determine the center {@link Point} for
+ */
 export const centerOf = async (target: Region | Promise<Region>): Promise<Point> => {
   const targetRegion = await target;
   const x = Math.floor(targetRegion.left + targetRegion.width / 2);
@@ -9,6 +13,10 @@ export const centerOf = async (target: Region | Promise<Region>): Promise<Point>
   return new Point(x, y);
 };
 
+/**
+ * {@link randomPointIn} returns a random {@link Point} within a given {@link Region}
+ * @param target {@link Region} the random {@link Point} has to be within
+ */
 export const randomPointIn = async (target: Region | Promise<Region>): Promise<Point> => {
   const targetRegion = await target;
   const x = Math.floor(targetRegion.left + Math.random() * targetRegion.width);
