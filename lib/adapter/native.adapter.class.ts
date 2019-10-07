f1c6e47849967ae2012244c0500cc9e3f7268e22
Simon Hofmann <mail@simon-hofmann.org>
2019-10-07 17:13:22 +0200

    (#87) NativeAdapter API docs

--- lib/adapter/native.adapter.class.ts
+++ lib/adapter/native.adapter.class.ts
@@ -9,13 +9,19 @@ import { KeyboardAction } from "../provider/native/robotjs-keyboard-action.class
 import { MouseAction } from "../provider/native/robotjs-mouse-action.class";
 
 /**
- * NativeAdapter serves as an abstraction layer for all OS level interactions.
+ * {@link NativeAdapter} serves as an abstraction layer for all OS level interactions.
  *
  * This allows to provide a high level interface for native actions,
- * whithout having to spread (possibly) multiple dependencies all over the code.
+ * without having to spread (possibly) multiple dependencies all over the code.
  * All actions which involve the OS are bundled in this adapter.
  */
 export class NativeAdapter {
+  /**
+   * {@link NativeAdapter} class constructor
+   * @param clipboard {@link ClipboardActionProvider} instance used to interact with a systems clipboard (Default: {@link ClipboardAction})
+   * @param keyboard {@link KeyboardActionProvider} instance used to interact with a systems keybaord (Default: {@link KeyboardAction})
+   * @param mouse {@link MouseActionInterface} instance used to interact with a systems mouse (Default: {@link MouseAction})
+   */
   constructor(
     private clipboard: ClipboardActionProvider = new ClipboardAction(),
     private keyboard: KeyboardActionProvider = new KeyboardAction(),
@@ -23,185 +29,165 @@ export class NativeAdapter {
   ) {}
 
   /**
-   * setMouseDelay configures mouse speed for movement
+   * {@link setMouseDelay} configures mouse speed for movement
    *
-   * @param {number} delay The delay
-   * @memberof NativeAdapter
+   * @param delay Mouse delay in milliseconds
    */
   public setMouseDelay(delay: number): void {
     this.mouse.setMouseDelay(delay);
   }
 
   /**
-   * setKeyboardDelay configures keyboard delay between key presses
+   * {@link setKeyboardDelay} configures keyboard delay between key presses
    *
-   * @param {number} delay The delay
-   * @memberof NativeAdapter
+   * @param delay The keyboard delay in milliseconds
    */
   public setKeyboardDelay(delay: number): void {
     this.keyboard.setKeyboardDelay(delay);
   }
 
   /**
-   * setMousePosition changes the current mouse cursor position to a given point
+   * {@link setMousePosition} changes the current mouse cursor position to a given {@link Point}
    *
-   * @param {Point} p The new cursor position
-   * @memberof NativeAdapter
+   * @param p The new cursor position at {@link Point} p
    */
   public setMousePosition(p: Point): Promise<void> {
     return this.mouse.setMousePosition(p);
   }
 
   /**
-   * getMousePosition returns the current mouse position
+   * {@link currentMousePosition} returns the current mouse position
    *
-   * @returns {Promise<Point>} Current cursor position
-   * @memberof NativeAdapter
+   * @returns Current cursor position at a certain {@link Point}
    */
   public currentMousePosition(): Promise<Point> {
     return this.mouse.currentMousePosition();
   }
 
   /**
-   * leftClick triggers a native left-click event via OS API
-   *
-   * @memberof NativeAdapter
+   * {@link leftClick} triggers a native left-click event via OS API
    */
   public leftClick(): Promise<void> {
     return this.mouse.leftClick();
   }
 
   /**
-   * rightClick triggers a native right-click event via OS API
-   *
-   * @memberof NativeAdapter
+   * {@link rightClick} triggers a native right-click event via OS API
    */
   public rightClick(): Promise<void> {
     return this.mouse.rightClick();
   }
 
   /**
-   * middleClick triggers a native middle-click event via OS API
+   * {@link middleClick} triggers a native middle-click event via OS API
    */
   public middleClick(): Promise<void> {
     return this.mouse.middleClick();
   }
 
   /**
-   * pressButton presses and holds a mouse button
+   * {@link pressButton} presses and holds a mouse {@link Button}
    *
-   * @param {Button} btn The mouse button to press
-   * @memberof NativeAdapter
+   * @param btn The mouse {@link Button} to press
    */
   public pressButton(btn: Button): Promise<void> {
     return this.mouse.pressButton(btn);
   }
 
   /**
-   * releaseButton releases a mouse button previously clicked via pressButton
+   * {@link releaseButton} releases a mouse {@link Button} previously clicked via {@link pressButton}
    *
-   * @param {Button} btn The mouse button to release
-   * @memberof NativeAdapter
+   * @param btn The mouse {@link Button} to release
    */
   public releaseButton(btn: Button): Promise<void> {
     return this.mouse.releaseButton(btn);
   }
 
   /**
-   * type types a given string via native keyboard events
+   * {@link type} types a given string via native keyboard events
    *
-   * @param {string} input The text to type
-   * @memberof NativeAdapter
+   * @param input The text to type
    */
   public type(input: string): Promise<void> {
     return this.keyboard.type(input);
   }
 
   /**
-   * click clicks a single Key via native keyboard event
+   * {@link click} clicks a {@link Key} via native keyboard event
    *
-   * @param {Key[]} keys The keys to click
-   * @memberof NativeAdapter
+   * @param keys Array of {@link Key}s to click
    */
   public click(...keys: Key[]): Promise<void> {
     return this.keyboard.click(...keys);
   }
 
   /**
-   * pressKey presses and holds a given Key
+   * {@link pressKey} presses and holds a given {@link Key}
    *
-   * @param {Key[]} keys The Keys to press and hold
-   * @memberof NativeAdapter
+   * @param keys Array of {@link Key}s to press and hold
    */
   public pressKey(...keys: Key[]): Promise<void> {
     return this.keyboard.pressKey(...keys);
   }
 
   /**
-   * releaseKey releases a Key previously presses via pressKey
+   * {@link releaseKey} releases a {@link Key} previously presses via {@link pressKey}
    *
-   * @param {Key[]} keys The Keys to release
-   * @memberof NativeAdapter
+   * @param keys Array of {@link Key}s to release
    */
   public releaseKey(...keys: Key[]): Promise<void> {
     return this.keyboard.releaseKey(...keys);
   }
 
   /**
-   * scrollUp triggers an upwards mouse wheel scroll
+   * {@link scrollUp} triggers an upwards mouse wheel scroll
    *
-   * @param {number} amount The amount of 'ticks' to scroll
-   * @memberof NativeAdapter
+   * @param amount The amount of 'ticks' to scroll
    */
   public scrollUp(amount: number): Promise<void> {
     return this.mouse.scrollUp(amount);
   }
 
   /**
-   * scrollDown triggers a downward mouse wheel scroll
+   * {@link scrollDown} triggers a downward mouse wheel scroll
    *
-   * @param {number} amount The amount of 'ticks' to scroll
-   * @memberof NativeAdapter
+   * @param amount The amount of 'ticks' to scroll
    */
   public scrollDown(amount: number): Promise<void> {
     return this.mouse.scrollDown(amount);
   }
 
   /**
-   * scrollLeft triggers a left mouse scroll
+   * {@link scrollLeft} triggers a left mouse scroll
    *
-   * @param {number} amount The amount of 'ticks' to scroll
-   * @memberof NativeAdapter
+   * @param amount The amount of 'ticks' to scroll
    */
   public scrollLeft(amount: number): Promise<void> {
     return this.mouse.scrollLeft(amount);
   }
 
   /**
-   * scrollRight triggers a right mouse scroll
+   * {@link scrollRight} triggers a right mouse scroll
    *
-   * @param {number} amount The amount of 'ticks' to scroll
-   * @memberof NativeAdapter
+   * @param amount The amount of 'ticks' to scroll
    */
   public scrollRight(amount: number): Promise<void> {
     return this.mouse.scrollRight(amount);
   }
 
   /**
-   * copy copies a given text to the system clipboard
+   * {@link copy} copies a given text to the system clipboard
    *
-   * @param {string} text The text to copy
-   * @memberof NativeAdapter
+   * @param text The text to copy
    */
   public copy(text: string): Promise<void> {
     return this.clipboard.copy(text);
   }
 
   /**
-   * paste pastes the current text on the system clipboard
+   * {@link paste} pastes the current text on the system clipboard
    *
-   * @returns {Promise<string>} The clipboard text
-   * @memberof NativeAdapter
+   * @returns The clipboard text
    */
   public paste(): Promise<string> {
     return this.clipboard.paste();
