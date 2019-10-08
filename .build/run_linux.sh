9fdd00a4c8eb4353f8eb6340aa8576e233a370a1
Simon Hofmann <mail@simon-hofmann.org>
2019-10-08 11:41:07 +0200

    Removed travis_retry from cmd

--- .build/run_linux.sh
+++ .build/run_linux.sh
@@ -3,4 +3,4 @@ set -e
 
 echo $PWD
 
-travis_retry docker exec nut-ci bash -c "bash $PWD/.build/build.sh ${PWD} ${NODE_VERSION}"
+docker exec nut-ci bash -c "bash $PWD/.build/build.sh ${PWD} ${NODE_VERSION}"
