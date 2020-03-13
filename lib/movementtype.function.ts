97cd7584c4bd6d8466c30822c04c7234b9c6a873
Simon Hofmann <mail@simon-hofmann.org>
2020-03-13 23:07:57 +0100

    (#85) Updated linear movementtype to return nanoseconds
    Nanoseconds instead of milliseconds allow for higher resolution

--- lib/movementtype.function.ts
+++ lib/movementtype.function.ts
@@ -1,14 +1,15 @@
 export const linear = (
-  amountOfSteps: number,
-  speedInPixelsPerSecond: number,
+    amountOfSteps: number,
+    speedInPixelsPerSecond: number,
 ): number[] => {
-  const timeSteps = [];
-  let stepDuration = Math.floor((1 / speedInPixelsPerSecond) * 1000);
-  if (stepDuration <= 0) {
-    stepDuration = 1;
-  }
-  for (let idx = 0; idx < amountOfSteps; ++idx) {
-    timeSteps.push(stepDuration);
-  }
-  return timeSteps;
+    const timeSteps = [];
+    // Duration per movement step in nanoseconds
+    let stepDuration = (1 / speedInPixelsPerSecond) * 1_000_000_000;
+    if (stepDuration <= 0) {
+        stepDuration = 0;
+    }
+    for (let idx = 0; idx < amountOfSteps; ++idx) {
+        timeSteps.push(stepDuration);
+    }
+    return timeSteps;
 };
