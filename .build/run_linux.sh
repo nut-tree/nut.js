dc17ff90ccf28f974069e7a57d15d72734fb86a8
Simon Hofmann <mail@simon-hofmann.org>
2020-03-15 20:03:04 +0100

    (#110) Switched to using node version env var provided by Travis

--- .build/run_linux.sh
+++ .build/run_linux.sh
@@ -3,4 +3,4 @@ set -e
 
 echo $PWD
 
-docker exec nut-ci bash -c "bash $PWD/.build/build.sh ${PWD} ${NODE_VERSION}"
+docker exec nut-ci bash -c "bash $PWD/.build/build.sh ${PWD} ${TRAVIS_NODE_VERSION}"
