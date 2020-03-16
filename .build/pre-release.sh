9d975a6c606f81053ef3e45fcdfccdd132269d5b
Simon Hofmann <mail@simon-hofmann.org>
2020-03-16 07:38:16 +0100

    Removed space from version string

--- .build/pre-release.sh
+++ .build/pre-release.sh
@@ -2,7 +2,7 @@
 
 npm ci
 patchVersion=$(npm --no-git-tag version patch)
-nextVersion=${patchVersion}-next."$(date +%Y%m%d%k%M%S)"
+nextVersion=${patchVersion}-next."$(date +%Y%m%d%H%M%S)"
 echo "${nextVersion:1}"
 
 npm version --no-git-tag -f "${nextVersion:1}"
