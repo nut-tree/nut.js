a4f01705fe658d18476f412de8e399a96f110d9b
Simon Hofmann <mail@simon-hofmann.org>
2020-05-19 16:03:48 +0200

    (#131) Refactored image-processor.class.ts to export functions instead of class

--- lib/provider/opencv/image-processor.class.ts
+++ lib/provider/opencv/image-processor.class.ts
@@ -1,55 +1,53 @@
 import * as cv from "opencv4nodejs-prebuilt";
-import { Image } from "../../image.class";
-import { Region } from "../../region.class";
+import {Image} from "../../image.class";
+import {Region} from "../../region.class";
 
 function determineROI(img: Image, roi: Region): cv.Rect {
-  return new cv.Rect(
-    Math.min(Math.max(roi.left, 0), img.width),
-    Math.min(Math.max(roi.top, 0), img.height),
-    Math.min(roi.width, img.width - roi.left),
-    Math.min(roi.height, img.height - roi.top));
+    return new cv.Rect(
+        Math.min(Math.max(roi.left, 0), img.width),
+        Math.min(Math.max(roi.top, 0), img.height),
+        Math.min(roi.width, img.width - roi.left),
+        Math.min(roi.height, img.height - roi.top));
 }
 
-export class ImageProcessor {
-  /**
-   * fromImageWithAlphaChannel should provide a way to create a library specific
-   * image with alpha channel from an abstract Image object holding raw data and image dimension
-   *
-   * @param {Image} img The input Image
-   * @param {Region} [roi] An optional Region to specify a ROI
-   * @returns {Promise<any>} An image
-   * @memberof VisionProviderInterface
-   */
-  public static async fromImageWithAlphaChannel(
+/**
+ * fromImageWithAlphaChannel should provide a way to create a library specific
+ * image with alpha channel from an abstract Image object holding raw data and image dimension
+ *
+ * @param {Image} img The input Image
+ * @param {Region} [roi] An optional Region to specify a ROI
+ * @returns {Promise<any>} An image
+ * @memberof VisionProviderInterface
+ */
+export const fromImageWithAlphaChannel = async (
     img: Image,
     roi?: Region,
-  ): Promise<cv.Mat> {
+): Promise<cv.Mat> => {
     const mat = await new cv.Mat(img.data, img.height, img.width, cv.CV_8UC4).cvtColorAsync(cv.COLOR_BGRA2BGR);
     if (roi) {
-      return mat.getRegion(determineROI(img, roi));
+        return mat.getRegion(determineROI(img, roi));
     } else {
-      return mat;
+        return mat;
     }
-  }
+};
 
-  /**
-   * fromImageWithoutAlphaChannel should provide a way to create a library specific
-   * image without alpha channel from an abstract Image object holding raw data and image dimension
-   *
-   * @param {Image} img The input Image
-   * @param {Region} [roi] An optional Region to specify a ROI
-   * @returns {Promise<any>} An image
-   * @memberof VisionProviderInterface
-   */
-  public static async fromImageWithoutAlphaChannel(
+/**
+ * fromImageWithoutAlphaChannel should provide a way to create a library specific
+ * image without alpha channel from an abstract Image object holding raw data and image dimension
+ *
+ * @param {Image} img The input Image
+ * @param {Region} [roi] An optional Region to specify a ROI
+ * @returns {Promise<any>} An image
+ * @memberof VisionProviderInterface
+ */
+export const fromImageWithoutAlphaChannel = async (
     img: Image,
     roi?: Region,
-  ): Promise<cv.Mat> {
+): Promise<cv.Mat> => {
     const mat = new cv.Mat(img.data, img.height, img.width, cv.CV_8UC3);
     if (roi) {
-      return mat.getRegion(determineROI(img, roi));
+        return mat.getRegion(determineROI(img, roi));
     } else {
-      return mat;
+        return mat;
     }
-  }
-}
+};
