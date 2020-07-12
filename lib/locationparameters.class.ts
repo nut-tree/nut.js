4e1f04ee29a78c2f6738f29a985ba381abf2b5be
Simon Hofmann <mail@simon-hofmann.org>
2020-07-12 22:29:10 +0200

    (#129) Updated docs comments to refer to new parameters class

--- lib/locationparameters.class.ts
+++ lib/locationparameters.class.ts
@@ -2,6 +2,7 @@ import {OptionalSearchParameters} from "./optionalsearchparameters.class";
 
 /**
  * {@deprecated This module serves as a polyfill to not break existing pre v1.5 code. Will be removed in v2.0.0}
+ * Use {@link OptionalSearchParameters} instead
  */
 export {
     OptionalSearchParameters as LocationParameters
