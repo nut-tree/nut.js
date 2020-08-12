3e2f2a9879b8ef5040ab7b3a532bd07390c4fcd0
Simon Hofmann <mail@simon-hofmann.org>
2020-08-12 16:11:52 +0200

    (#5) Added test for window class

---
+++ lib/window.class.spec.ts
@@ -0,0 +1,34 @@
+import {Window} from "./window.class";
+import {NativeAdapter} from "./adapter/native.adapter.class";
+
+jest.mock("./adapter/native.adapter.class");
+
+describe("Window class", () => {
+    it("should retrieve the window region via its native adapter", async () => {
+        // GIVEN
+        const nativeAdapterMock = new NativeAdapter();
+        const mockWindowHandle = 123;
+        const SUT = new Window(nativeAdapterMock, mockWindowHandle);
+
+        // WHEN
+        await SUT.region
+
+        // THEN
+        expect(nativeAdapterMock.getWindowRegion).toBeCalledTimes(1);
+        expect(nativeAdapterMock.getWindowRegion).toBeCalledWith(mockWindowHandle);
+    });
+
+    it("should retrieve the window title via its native adapter", async () => {
+        // GIVEN
+        const nativeAdapterMock = new NativeAdapter();
+        const mockWindowHandle = 123;
+        const SUT = new Window(nativeAdapterMock, mockWindowHandle);
+
+        // WHEN
+        await SUT.title
+
+        // THEN
+        expect(nativeAdapterMock.getWindowTitle).toBeCalledTimes(1);
+        expect(nativeAdapterMock.getWindowTitle).toBeCalledWith(mockWindowHandle);
+    });
+});
