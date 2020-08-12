373d39d9233dc82c5a17abf94a085dea94b59e14
Simon Hofmann <mail@simon-hofmann.org>
2020-08-12 11:20:00 +0200

    (#5) Increased shm size for Docker container

--- .build/setup_linux.sh
+++ .build/setup_linux.sh
@@ -6,4 +6,4 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update
 sudo apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce
 docker pull s1hofmann/nut-ci:latest
-docker run -it -d --name nut-ci --user $(id -u):$(id -g) -v ${PWD}:${PWD}:rw s1hofmann/nut-ci:latest bash
+docker run -it -d --name nut-ci --shm-size 4gb --user $(id -u):$(id -g) -v ${PWD}:${PWD}:rw s1hofmann/nut-ci:latest bash
