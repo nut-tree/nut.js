dc9cb4aff5e44b0a233093c8e56d0ad022588f40
Simon Hofmann <mail@simon-hofmann.org>
2020-08-12 13:44:54 +0200

    (#5) Updated coverage merging to use istanbul-merge and nyc

--- .build/run_linux.sh
+++ .build/run_linux.sh
@@ -9,6 +9,8 @@ npm run compile
 echo "init e2e test subpackage"
 npm --prefix e2e/tests ci
 echo "npm run coverage"
+npm run coverage:clean
 npm run coverage -- --coverageDirectory=coverage/unit
 docker exec nut-ci bash -c "bash $PWD/.build/build.sh ${PWD} ${TRAVIS_NODE_VERSION}"
-npm run coverage:collect
+npm run coverage:merge
+npm run coverage:merge-report
