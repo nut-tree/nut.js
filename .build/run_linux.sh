9175e6ca698db6ddaafa6f0f447780298cc21865
Simon Hofmann <mail@simon-hofmann.org>
2020-08-12 12:11:57 +0200

    (#5) Enabled xvfb on Linux

--- .build/run_linux.sh
+++ .build/run_linux.sh
@@ -9,5 +9,5 @@ npm run compile
 echo "init e2e test subpackage"
 npm --prefix e2e/tests ci
 echo "npm run coverage"
-E2E_TEST=1 npm run coverage
+npm run coverage
 docker exec nut-ci bash -c "bash $PWD/.build/build.sh ${PWD} ${TRAVIS_NODE_VERSION}"
