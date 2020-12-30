538c8a67eb12fc872a47fa0ba63204a7dca8949c
Simon Hofmann <mail@simon-hofmann.org>
2020-12-30 20:58:01 +0100

    Enabled matrix build and release jobs

--- .build/pre-release.sh
+++ .build/pre-release.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -ex
 
-npm ci
-patchVersion=$(npm --no-git-tag version patch)
-nextVersion=${patchVersion}-next."$(date +%Y%m%d%H%M%S)"
-echo "${nextVersion:1}"
+echo "snapshot release"
 
-npm version --no-git-tag -f "${nextVersion:1}"
-npm run publish-next
+#npm ci
+#patchVersion=$(npm --no-git-tag version patch)
+#nextVersion=${patchVersion}-next."$(date +%Y%m%d%H%M%S)"
+#echo "${nextVersion:1}"
+#
+#npm version --no-git-tag -f "${nextVersion:1}"
+#npm run publish-next
