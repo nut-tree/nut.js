96d122c685296909f25beaa6b8d50597a35b2023
Simon Hofmann <mail@simon-hofmann.org>
2021-01-07 00:01:48 +0100

    Working cross-platform setup for CI, snapshot releases as well as tagged releases

--- .build/pre-release.sh
+++ .build/pre-release.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -ex
 
-echo "snapshot release"
+echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
+npm whoami
 
-#npm ci
-#patchVersion=$(npm --no-git-tag version patch)
-#nextVersion=${patchVersion}-next."$(date +%Y%m%d%H%M%S)"
-#echo "${nextVersion:1}"
-#
-#npm version --no-git-tag -f "${nextVersion:1}"
-#npm run publish-next
+npm ci
+patchVersion=$(npm --no-git-tag version patch)
+nextVersion=${patchVersion}-next."$(date +%Y%m%d%H%M%S)"
+echo "${nextVersion:1}"
+
+npm version --no-git-tag -f "${nextVersion:1}"
+npm run publish-next
