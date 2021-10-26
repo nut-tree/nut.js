49b9c6d89fdeb70b37e130f7ce0a40c31b4cd522
Simon Hofmann <mail@simon-hofmann.org>
2021-10-26 23:55:47 +0200

    (#279) Add error info to output, formatting

--- lib/assert.class.ts
+++ lib/assert.class.ts
@@ -1,36 +1,37 @@
-import { LocationParameters } from "./locationparameters.class";
-import { Region } from "./region.class";
-import { ScreenClass } from "./screen.class";
+import {LocationParameters} from "./locationparameters.class";
+import {Region} from "./region.class";
+import {ScreenClass} from "./screen.class";
 
 export class AssertClass {
-  constructor(private screen: ScreenClass) {}
-
-  public async isVisible(pathToNeedle: string, searchRegion?: Region, confidence?: number) {
-    try {
-      await this.screen.find(
-        pathToNeedle,
-        {searchRegion, confidence} as LocationParameters,
-      );
-    } catch (err) {
-      if (searchRegion !== undefined) {
-        throw new Error(
-          `Element '${pathToNeedle}' not found in region ${searchRegion.toString()}`,
-        );
-      } else {
-        throw new Error(`Element '${pathToNeedle}' not found`);
-      }
+    constructor(private screen: ScreenClass) {
     }
-  }
 
-  public async notVisible(pathToNeedle: string, searchRegion?: Region, confidence?: number) {
-    try {
-      await this.screen.find(
-        pathToNeedle,
-        {searchRegion, confidence} as LocationParameters,
-      );
-    } catch (err) {
-      return;
+    public async isVisible(pathToNeedle: string, searchRegion?: Region, confidence?: number) {
+        try {
+            await this.screen.find(
+                pathToNeedle,
+                {searchRegion, confidence} as LocationParameters,
+            );
+        } catch (err) {
+            if (searchRegion !== undefined) {
+                throw new Error(
+                    `Element '${pathToNeedle}' not found in region ${searchRegion.toString()}. Reason: ${err}`,
+                );
+            } else {
+                throw new Error(`Element '${pathToNeedle}' not found. Reason: ${err}`);
+            }
+        }
     }
-    throw new Error(`'${pathToNeedle}' is visible`);
-  }
+
+    public async notVisible(pathToNeedle: string, searchRegion?: Region, confidence?: number) {
+        try {
+            await this.screen.find(
+                pathToNeedle,
+                {searchRegion, confidence} as LocationParameters,
+            );
+        } catch (err) {
+            return;
+        }
+        throw new Error(`'${pathToNeedle}' is visible`);
+    }
 }
