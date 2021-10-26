ef98e447a9f32aefdfcb9d59088b3c40536a1c72
Simon Hofmann <s1hofmann@users.noreply.github.com>
2021-10-26 22:06:14 +0200

    (#296) Add missing export for ImageWriterParameters (#297)

--- lib/provider/index.ts
+++ lib/provider/index.ts
@@ -3,7 +3,7 @@ export {DataSinkInterface} from "./data-sink.interface";
 export {DataSourceInterface} from "./data-source.interface";
 export {ImageFinderInterface} from "./image-finder.interface";
 export {ImageReader} from "./image-reader.type";
-export {ImageWriter} from "./image-writer.type";
+export {ImageWriter, ImageWriterParameters} from "./image-writer.type";
 export {KeyboardProviderInterface} from "./keyboard-provider.interface";
 export {MouseProviderInterface} from "./mouse-provider.interface";
 export {ScreenProviderInterface} from "./screen-provider.interface";
