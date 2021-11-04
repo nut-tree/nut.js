99440cc199e7d5a741470f0f63c0a834d5136c20
Simon Hofmann <mail@simon-hofmann.org>
2021-11-04 22:36:51 +0100

    (#307) Stick to BGR color format when loading images

--- lib/provider/io/jimp-image-reader.class.ts
+++ lib/provider/io/jimp-image-reader.class.ts
@@ -7,6 +7,12 @@ export default class implements ImageReader {
         return new Promise<Image>((resolve, reject) => {
             Jimp.read(parameters)
                 .then(jimpImage => {
+                    // stay consistent with images retrieved from libnut which uses BGR format
+                    jimpImage.scan(0, 0, jimpImage.bitmap.width, jimpImage.bitmap.height, function(_, __, idx) {
+                        const red = this.bitmap.data[idx];
+                        this.bitmap.data[idx] = this.bitmap.data[idx + 2];
+                        this.bitmap.data[idx + 2] = red;
+                    });
                     resolve(new Image(
                         jimpImage.bitmap.width,
                         jimpImage.bitmap.height,
