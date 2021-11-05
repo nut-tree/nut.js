694643859a29a4cc5b81da094d168b0b96e9d4e2
Simon Hofmann <mail@simon-hofmann.org>
2021-11-06 00:01:16 +0100

    (#310) Migrated window.class to not use adapters

--- lib/window.class.spec.ts
+++ lib/window.class.spec.ts
@@ -1,36 +1,51 @@
 import {Window} from "./window.class";
-import {NativeAdapter} from "./adapter/native.adapter.class";
-import providerRegistry from "./provider/provider-registry.class";
+import {ProviderRegistry} from "./provider/provider-registry.class";
+import {mockPartial} from "sneer";
+import {WindowProviderInterface} from "./provider";
 
-jest.mock('jimp', () => {});
-jest.mock("./adapter/native.adapter.class");
+jest.mock('jimp', () => {
+});
 
 describe("Window class", () => {
     it("should retrieve the window region via its native adapter", async () => {
         // GIVEN
-        const nativeAdapterMock = new NativeAdapter(providerRegistry);
+        const windowMock = jest.fn();
+        const providerRegistryMock = mockPartial<ProviderRegistry>({
+            getWindow(): WindowProviderInterface {
+                return mockPartial<WindowProviderInterface>({
+                    getWindowRegion: windowMock
+                })
+            }
+        })
         const mockWindowHandle = 123;
-        const SUT = new Window(nativeAdapterMock, mockWindowHandle);
+        const SUT = new Window(providerRegistryMock, mockWindowHandle);
 
         // WHEN
         await SUT.region
 
         // THEN
-        expect(nativeAdapterMock.getWindowRegion).toBeCalledTimes(1);
-        expect(nativeAdapterMock.getWindowRegion).toBeCalledWith(mockWindowHandle);
+        expect(windowMock).toBeCalledTimes(1);
+        expect(windowMock).toBeCalledWith(mockWindowHandle);
     });
 
     it("should retrieve the window title via its native adapter", async () => {
         // GIVEN
-        const nativeAdapterMock = new NativeAdapter(providerRegistry);
+        const windowMock = jest.fn();
+        const providerRegistryMock = mockPartial<ProviderRegistry>({
+            getWindow(): WindowProviderInterface {
+                return mockPartial<WindowProviderInterface>({
+                    getWindowTitle: windowMock
+                })
+            }
+        })
         const mockWindowHandle = 123;
-        const SUT = new Window(nativeAdapterMock, mockWindowHandle);
+        const SUT = new Window(providerRegistryMock, mockWindowHandle);
 
         // WHEN
         await SUT.title
 
         // THEN
-        expect(nativeAdapterMock.getWindowTitle).toBeCalledTimes(1);
-        expect(nativeAdapterMock.getWindowTitle).toBeCalledWith(mockWindowHandle);
+        expect(windowMock).toBeCalledTimes(1);
+        expect(windowMock).toBeCalledWith(mockWindowHandle);
     });
 });
