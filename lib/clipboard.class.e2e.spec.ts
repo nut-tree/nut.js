c5f8848684466dc41ab45021b78b9a2a04164116
Simon Hofmann <mail@simon-hofmann.org>
2021-11-06 21:21:18 +0100

    (#310) Migrated clipboard.class to not use adapters

--- lib/clipboard.class.e2e.spec.ts
+++ lib/clipboard.class.e2e.spec.ts
@@ -1,11 +1,9 @@
-import {NativeAdapter} from "./adapter/native.adapter.class";
 import {ClipboardClass} from "./clipboard.class";
 import providerRegistry from "./provider/provider-registry.class";
 
 describe("Clipboard class", () => {
     it("should paste copied input from system clipboard.", async () => {
-        const adapterMock = new NativeAdapter(providerRegistry);
-        const SUT = new ClipboardClass(adapterMock);
+        const SUT = new ClipboardClass(providerRegistry);
 
         const textToCopy = "bar";
 
