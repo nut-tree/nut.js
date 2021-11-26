d98aa7117897cc03cefa11bea76a4e2dbfdd0f5f
Simon Hofmann <mail@simon-hofmann.org>
2021-11-26 13:31:15 +0100

    (#204) Adapted toShow matcher to new find parameters

--- lib/expect/matchers/toShow.function.ts
+++ lib/expect/matchers/toShow.function.ts
@@ -1,26 +1,27 @@
-import { LocationParameters } from "../../locationparameters.class";
-import { ScreenClass } from "../../screen.class";
+import {LocationParameters} from "../../locationparameters.class";
+import {ScreenClass} from "../../screen.class";
+import {Image} from "../../image.class";
 
 export const toShow = async (
-  received: ScreenClass,
-  needle: string,
-  confidence?: number,
+    received: ScreenClass,
+    needle: string | Image,
+    confidence?: number,
 ) => {
-  let locationParams;
-  if (confidence) {
-    locationParams = new LocationParameters();
-    locationParams.confidence = confidence;
-  }
-  try {
-    await received.find(needle, locationParams);
-    return {
-      message: () => `Expected screen to not show ${needle}`,
-      pass: true,
-    };
-  } catch (err) {
-    return {
-      message: () => `Screen is not showing ${needle}: ${err}`,
-      pass: false,
-    };
-  }
+    let locationParams;
+    if (confidence) {
+        locationParams = new LocationParameters();
+        locationParams.confidence = confidence;
+    }
+    try {
+        await received.find(needle, locationParams);
+        return {
+            message: () => `Expected screen to not show ${needle}`,
+            pass: true,
+        };
+    } catch (err) {
+        return {
+            message: () => `Screen is not showing ${needle}: ${err}`,
+            pass: false,
+        };
+    }
 };
