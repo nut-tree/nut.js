5610e6f39f28dda61e094052c9c4dce28ac387fa
Simon Hofmann <mail@simon-hofmann.org>
2021-12-10 23:12:51 +0100

    (#336) Extended Image class with color mode conversion methods

--- lib/image.class.ts
+++ lib/image.class.ts
@@ -1,3 +1,6 @@
+import {imageToJimp} from "./provider/io/imageToJimp.function";
+import {ColorMode} from "./colormode.enum";
+
 /**
  * The {@link Image} class represents generic image data
  */
@@ -9,6 +12,7 @@ export class Image {
      * @param data Generic {@link Image} data
      * @param channels Amount of {@link Image} channels
      * @param id Image identifier
+     * @param colorMode An images color mode, defaults to {@link ColorMode.BGR}
      * @param pixelDensity Object containing scale info to work with e.g. Retina display data where the reported display size and pixel size differ (Default: {scaleX: 1.0, scaleY: 1.0})
      */
     constructor(
@@ -17,6 +21,7 @@ export class Image {
         public readonly data: any,
         public readonly channels: number,
         public readonly id: string,
+        public readonly colorMode: ColorMode = ColorMode.BGR,
         public readonly pixelDensity: { scaleX: number; scaleY: number } = {
             scaleX: 1.0,
             scaleY: 1.0,
@@ -32,5 +37,36 @@ export class Image {
      */
     public get hasAlphaChannel() {
         return this.channels > 3;
+    }
+
+    /**
+     * {@link toRGB} converts an {@link Image} from BGR color mode (default within nut.js) to RGB
+     */
+    public async toRGB(): Promise<Image> {
+        if (this.colorMode === ColorMode.RGB) {
+            return this;
+        }
+        const rgbImage = imageToJimp(this);
+        return new Image(this.width, this.height, rgbImage.bitmap.data, this.channels, this.id, ColorMode.RGB, this.pixelDensity);
+    }
+
+    /**
+     * {@link toBGR} converts an {@link Image} from RGB color mode to RGB
+     */
+    public async toBGR(): Promise<Image> {
+        if (this.colorMode === ColorMode.BGR) {
+            return this;
+        }
+        const rgbImage = imageToJimp(this);
+        return new Image(this.width, this.height, rgbImage.bitmap.data, this.channels, this.id, ColorMode.BGR, this.pixelDensity);
+    }
+
+    /**
+     * {@link fromRGBData} creates an {@link Image} from provided RGB data
+     */
+    public static fromRGBData(width: number, height: number, data: Buffer, channels: number, id: string): Image {
+        const rgbImage = new Image(width, height, data, channels, id);
+        const jimpImage = imageToJimp(rgbImage);
+        return new Image(width, height, jimpImage.bitmap.data, channels, id);
     }
 }
