33bfd38be618eb2ff67c626e9063a1cad634ce09
Simon Hofmann <mail@simon-hofmann.org>
2022-05-06 21:54:51 +0200

    (#400) Implement fetchFromUrl + tests

--- lib/imageResources.function.spec.ts
+++ lib/imageResources.function.spec.ts
@@ -1,8 +1,9 @@
-import {loadImageResource} from "./imageResources.function";
+import {fetchFromUrl, loadImageResource} from "./imageResources.function";
 import {mockPartial} from "sneer";
 import {ProviderRegistry} from "./provider/provider-registry.class";
 import {ImageReader} from "./provider";
 import {join} from "path";
+import {ColorMode} from "./colormode.enum";
 
 const loadMock = jest.fn();
 const providerRegistryMock = mockPartial<ProviderRegistry>({
@@ -24,5 +25,47 @@ describe('imageResources', () => {
 
         // THEN
         expect(loadMock).toBeCalledWith(join(resourceDirectoryPath, imageFileName));
+    });
+});
+
+describe('fetchFromUrl', () => {
+    it('should throw on malformed URLs', async () => {
+        // GIVEN
+        const malformedUrl = "foo";
+
+        // WHEN
+        const SUT = () => fetchFromUrl(malformedUrl);
+
+        // THEN
+        await expect(SUT).rejects.toThrowError("Invalid URL");
+    });
+
+    it('should throw on non-image URLs', async () => {
+        // GIVEN
+        const nonImageUrl = 'https://www.npmjs.com/package/jimp';
+
+        // WHEN
+        const SUT = () => fetchFromUrl(nonImageUrl);
+
+        // THEN
+        await expect(SUT).rejects.toThrowError('Could not find MIME for Buffer');
+    });
+
+    it('should return an RGB image from a valid URL', async () => {
+        // GIVEN
+        const validImageUrl = 'https://github.com/nut-tree/nut.js/raw/master/.gfx/nut.png';
+        const expectedDimensions = {
+            width: 502,
+            height: 411
+        };
+        const expectedColorMode = ColorMode.RGB;
+
+        // WHEN
+        const rgbImage = await fetchFromUrl(validImageUrl);
+
+        // THEN
+        expect(rgbImage.colorMode).toBe(expectedColorMode);
+        expect(rgbImage.width).toBe(expectedDimensions.width);
+        expect(rgbImage.height).toBe(expectedDimensions.height);
     });
 });
--- lib/imageResources.function.ts
+++ lib/imageResources.function.ts
@@ -1,7 +1,38 @@
 import {join, normalize} from "path";
 import {ProviderRegistry} from "./provider/provider-registry.class";
+import {URL} from "url";
+import {Image} from "./image.class";
+import Jimp from "jimp";
+import {ColorMode} from "./colormode.enum";
 
 export function loadImageResource(providerRegistry: ProviderRegistry, resourceDirectory: string, fileName: string) {
     const fullPath = normalize(join(resourceDirectory, fileName));
     return providerRegistry.getImageReader().load(fullPath);
+}
+
+export async function fetchFromUrl(url: string | URL): Promise<Image> {
+    let imageUrl: URL;
+    if (url instanceof URL) {
+        imageUrl = url;
+    } else {
+        try {
+            imageUrl = new URL(url);
+        } catch (e) {
+            throw e;
+        }
+    }
+    return Jimp.read(imageUrl.href)
+        .then((image) => {
+            return new Image(
+                image.bitmap.width,
+                image.bitmap.height,
+                image.bitmap.data,
+                4,
+                imageUrl.href,
+                ColorMode.RGB
+            );
+        })
+        .catch(err => {
+            throw err;
+        });
 }
