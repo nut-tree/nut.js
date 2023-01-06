4e3b18037023c31eca50ce43a384bf16d1fe6c67
Simon Hofmann <mail@simon-hofmann.org>
2023-01-06 23:00:35 +0100

    (#455) BREAKING: Remove both searchMultipleScales and confidence from OptionalSearchParameters since these properties would be provider specific and should be handled via providerData

--- lib/optionalsearchparameters.class.ts
+++ lib/optionalsearchparameters.class.ts
@@ -2,20 +2,18 @@ import { Region } from "./region.class";
 import { AbortSignal } from "node-abort-controller";
 
 /**
- * {@link OptionalSearchParameters} serves as a data class holding location parameters for image search
+ * {@link OptionalSearchParameters} serves as a data class holding optional parameters for search purposes
  */
-export class OptionalSearchParameters {
+export class OptionalSearchParameters<PROVIDER_DATA_TYPE> {
   /**
    * {@link OptionalSearchParameters} class constructor
    * @param searchRegion Optional {@link Region} to limit the search space to
-   * @param confidence Optional confidence value to configure image match confidence
-   * @param searchMultipleScales Optional flag to indicate if the search should be conducted at different scales
    * @param abort An {@link AbortSignal} to cancel an ongoing call to `waitFor`
+   * @param providerData Optional data that gets passed onto the provider implementation
    */
   constructor(
-    public searchRegion?: Region,
-    public confidence?: number,
-    public searchMultipleScales?: boolean,
-    public abort?: AbortSignal
+    public searchRegion?: Region | Promise<Region>,
+    public abort?: AbortSignal,
+    public providerData?: PROVIDER_DATA_TYPE
   ) {}
 }
