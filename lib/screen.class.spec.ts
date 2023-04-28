c6c309ee06395a064adf6711b6905ecb549b6f18
Simon Hofmann <mail@simon-hofmann.org>
2023-04-28 18:44:43 +0200

    (#500) Add test for the new color-finder queries in screen.class.spec.ts

--- lib/screen.class.spec.ts
+++ lib/screen.class.spec.ts
@@ -639,10 +639,10 @@ describe("Screen.", () => {
           },
         };
 
-        const findMatchMock = jest.fn(() => Promise.resolve(matchResult));
+        const findMatchMock = jest.fn(() => Promise.resolve([matchResult]));
         providerRegistryMock.getColorFinder = jest.fn(() =>
           mockPartial<ColorFinderInterface>({
-            findMatch: findMatchMock,
+            findMatches: findMatchMock,
           })
         );
         providerRegistryMock.getLogProvider = () => new NoopLogProvider();
