153857669b6d6f712315ca185c727af867e114e2
Simon Hofmann <mail@simon-hofmann.org>
2023-12-15 00:19:50 +0100

    (#547) Get rid of Docker tests

--- lib/expect/matchers/toBeAt.function.spec.ts
+++ lib/expect/matchers/toBeAt.function.spec.ts
@@ -2,21 +2,17 @@ import { mouse } from "../../../index";
 import { Point } from "../../point.class";
 import { toBeAt } from "./toBeAt.function";
 
-jest.mock("jimp", () => {});
-
 const targetPoint = new Point(100, 100);
 
 describe(".toBeAt", () => {
-  beforeEach(() => {
-    mouse.setPosition(targetPoint);
-  });
-
   it("should succeed when cursor is at right position.", async () => {
+    mouse.getPosition = jest.fn(() => Promise.resolve(targetPoint));
     const result = await toBeAt(mouse, targetPoint);
     expect(result.pass).toBeTruthy();
   });
 
   it("should fail when cursor is at wrong position.", async () => {
+    mouse.getPosition = jest.fn(() => Promise.resolve(targetPoint));
     const result = await toBeAt(mouse, new Point(10, 10));
     expect(result.pass).toBeFalsy();
   });
