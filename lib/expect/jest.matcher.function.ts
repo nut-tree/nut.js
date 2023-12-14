c1b8e44e0caaf22aa07045a4ddcaa9a1463b4617
Simon Hofmann <mail@simon-hofmann.org>
2023-12-14 20:46:09 +0100

    (#547) Expose new matchers

--- lib/expect/jest.matcher.function.ts
+++ lib/expect/jest.matcher.function.ts
@@ -4,6 +4,9 @@ import { toBeAt } from "./matchers/toBeAt.function";
 import { toBeIn } from "./matchers/toBeIn.function";
 import { toShow } from "./matchers/toShow.function";
 import { FindInput } from "../screen.class";
+import { RGBA } from "../rgba.class";
+import { toHaveColor } from "./matchers/toHaveColor.function";
+import { OptionalSearchParameters } from "../optionalsearchparameters.class";
 
 declare global {
   namespace jest {
@@ -11,10 +14,11 @@ declare global {
       toBeAt: (position: Point) => ReturnType<typeof toBeAt>;
       toBeIn: (region: Region) => ReturnType<typeof toBeIn>;
       // @ts-ignore
-      toShow: (
+      toShow: <PROVIDER_DATA>(
         needle: FindInput,
-        confidence?: number
+        parameters?: OptionalSearchParameters<PROVIDER_DATA>,
       ) => ReturnType<typeof toShow>;
+      toHaveColor: (color: RGBA) => ReturnType<typeof toHaveColor>;
     }
   }
 }
@@ -23,4 +27,5 @@ export const jestMatchers = {
   toBeAt,
   toBeIn,
   toShow,
+  toHaveColor,
 };
