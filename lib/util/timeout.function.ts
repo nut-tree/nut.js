616147580b18fe68199dfc24a8c1ea6f984ce38e
Simon Hofmann <mail@simon-hofmann.org>
2024-02-03 23:20:04 +0100

    (#556) Store intermediate results and errors to provide additional context on timeout

--- lib/util/timeout.function.ts
+++ lib/util/timeout.function.ts
@@ -8,11 +8,13 @@ export function timeout<R>(
   updateIntervalMs: number,
   maxDurationMs: number,
   action: (...params: any) => Promise<R>,
-  config?: TimoutConfig
+  config?: TimoutConfig,
 ): Promise<R> {
   return new Promise<R>((resolve, reject) => {
     let interval: NodeJS.Timeout;
     let timerCleaned = false;
+    let lastResult: R | null;
+    let lastRejectionReason: any | null;
 
     if (config?.signal) {
       config.signal.onabort = () => {
@@ -29,12 +31,16 @@ export function timeout<R>(
       if (!result && !timerCleaned) {
         interval = setTimeout(executeInterval, updateIntervalMs);
       } else {
+        lastResult = result;
+        lastRejectionReason = null;
         cleanupTimer();
         resolve(result);
       }
     }
 
-    function handleRejection() {
+    function handleRejection(reason: any) {
+      lastRejectionReason = reason;
+      lastResult = null;
       if (!timerCleaned) {
         interval = setTimeout(executeInterval, updateIntervalMs);
       }
@@ -52,7 +58,17 @@ export function timeout<R>(
 
     const maxTimeout = setTimeout(() => {
       cleanupTimer();
-      reject(`Action timed out after ${maxDurationMs} ms`);
+      let additionalInformation: string | undefined;
+      if (lastResult == null && lastRejectionReason != null) {
+        additionalInformation = `Last rejection reason was: ${lastRejectionReason}.`;
+      } else if (lastResult == null && lastRejectionReason == null) {
+        additionalInformation = `Didn't receive a result within timeout.`;
+      }
+      reject(
+        `Action timed out after ${maxDurationMs} ms.${
+          additionalInformation ? ` ${additionalInformation}` : ""
+        }`,
+      );
     }, maxDurationMs);
 
     executeInterval();
