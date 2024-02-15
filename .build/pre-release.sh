e81bddc37ada35810ad8ac949c943818b29c6e35
Simon Hofmann <mail@simon-hofmann.org>
2024-02-15 21:14:28 +0100

    Fix snapshot release workflow

--- .build/pre-release.sh
+++ .build/pre-release.sh
@@ -5,4 +5,4 @@ patchVersion=$(npm --no-git-tag version patch)
 nextVersion=${patchVersion}-next."$(date +%Y%m%d%H%M%S)"
 echo "${nextVersion:1}"
 
-node ./bump_version.js "${nextVersion:1}"
+node ./.build/bump_version.js "${nextVersion:1}"
