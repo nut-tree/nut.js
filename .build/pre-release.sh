323139f04060a6383db2763fa096771b37ff504a
Simon Hofmann <mail@simon-hofmann.org>
2024-02-15 20:54:08 +0100

    Aligned package versions, added script to bump versions on snapshot releases

--- .build/pre-release.sh
+++ .build/pre-release.sh
@@ -5,4 +5,4 @@ patchVersion=$(npm --no-git-tag version patch)
 nextVersion=${patchVersion}-next."$(date +%Y%m%d%H%M%S)"
 echo "${nextVersion:1}"
 
-npm version --no-git-tag -f "${nextVersion:1}"
+node ./bump_version.js "${nextVersion:1}"
